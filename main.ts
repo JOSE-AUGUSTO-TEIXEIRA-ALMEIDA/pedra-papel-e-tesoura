82041f143161a712aad3be9fda38e5fd1903cb54
Guto <111243404+JOSE-AUGUSTO-TEIXEIRA-ALMEIDA@users.noreply.github.com>
2024-04-04 11:27:50 -0300

    Acréscimo de sons à programação pedra, papel e tesoura.

--- main.ts
+++ main.ts
@@ -6,9 +6,15 @@ input.onGesture(Gesture.Shake, function () {
     x = randint(0, 2)
     if (x == 0) {
         basic.showIcon(IconNames.SmallSquare)
+        basic.pause(1000)
+        music.play(music.builtinPlayableSoundEffect(soundExpression.mysterious), music.PlaybackMode.UntilDone)
     } else if (x == 1) {
         basic.showIcon(IconNames.Square)
+        basic.pause(1000)
+        music.play(music.builtinPlayableSoundEffect(soundExpression.soaring), music.PlaybackMode.UntilDone)
     } else {
         basic.showIcon(IconNames.Scissors)
+        basic.pause(1000)
+        music.play(music.builtinPlayableSoundEffect(soundExpression.twinkle), music.PlaybackMode.UntilDone)
     }
 })
